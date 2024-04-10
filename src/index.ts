4fab2d63b9630a1e521d6f987954c4b64e2499b8
Jeremiah <jeremiahjacobson10@gmail.com>
2024-04-09 22:38:14 -0400

    added status icon for status

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,21 @@ import "./styles.css";
 async function displayWeatherData(locationName: string) {
   const result = await retrieveWeatherData(locationName);
 
+  const weatherStatus = document.querySelector("#weather-status");
+  const statusIcon = document.querySelector("#status-icon") as HTMLImageElement;
+
   if (result.error) {
     console.error(`An error occurred: ${result.error}`);
     // Display error message to the user
-    document.querySelector(
-      "#weather-status"
-    ).textContent = `Error fetching weather data: ${result.error}`;
+    if (weatherStatus) {
+      weatherStatus.textContent = `Error fetching weather data: ${result.error}`;
+    }
   } else {
     console.log("Weather data:", result.weatherLocation);
     // Display weather data to the user
-    document.querySelector(
-      "#weather-status"
-    ).textContent = `Weather in ${result.weatherLocation.city}: ${result.weatherLocation.conditionText}`;
+    weatherStatus.textContent = `Weather in ${result.weatherLocation.city}: ${result.weatherLocation.conditionText}`;
+    // show appropriate weather icon
+    statusIcon.src = result.weatherLocation.conditionIcon;
   }
 }
 
